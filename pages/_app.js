2a8efd434f185c3e044e9de4a4516015b3056824
Dawidx3p <maildawcio@gmail.com>
2022-08-18 08:26:44 +0200

    add questions component and routing for specific question

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import Header from '../components/Header';
 
 const GlobalStyle2 = createGlobalStyle`
   body {
@@ -18,6 +19,7 @@ const GlobalStyle2 = createGlobalStyle`
 function MyApp({ Component, pageProps }) {
   return (<>
     <GlobalStyle2 />
+    <Header />
     <Component {...pageProps} />
   </>)
 }
--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import Card from '../../components/Card';
+
 const QuestionsContainer = styled.div`
     display: flex;
     justify-content: space-between;
@@ -6,9 +9,36 @@ const QuestionsContainer = styled.div`
     margin: 5%;
 `;
 function Questions() {
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    useEffect(() => {  
+        async function fetchData() {    
+            const data = await fetch('https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow');
+            const result = await data.json();     
+            if (result) {      
+                setQuestions(result.items);       
+                setLoading(false);  
+            }
+        }
+        fetchData();
+    }, []);
     return (
     <QuestionsContainer>
         <h2>Questions</h2>
+        {loading ? (
+        <span>Loading...</span>
+        ) : (
+        <div>    
+            {questions.map((question) => (      
+                <Card        
+                key={question.question_id}        
+                title={question.title}        
+                views={question.view_count}        
+                answers={question.answer_count}      
+                />    
+                ))}
+                </div> 
+                )}
     </QuestionsContainer>
     );
 }
