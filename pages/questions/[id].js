fe2f46b809ae321d48b21f3e25866bdc4d2a3cbb
Dawidx3p <maildawcio@gmail.com>
2022-08-18 08:44:06 +0200

    add fetching logic and elements to render the data to the [id].js

--- pages/questions/[id].js
+++ pages/questions/[id].js
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import Card from '../../components/Card';
+
 const QuestionDetailContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -9,9 +12,30 @@ const QuestionDetailContainer = styled.div`
 function QuestionDetail() {
   const router = useRouter();
   const { id } = router.query;
+  const [loading, setLoading] = useState(false);
+  const [question, setQuestion] = useState({});
+  useEffect(() => {
+    async function fetchData(){
+        const data = await fetchData('https://api.stackexchange.com/2.2/questions/${id}?site=stackoverflow');
+        const result = await data.json();
+        if(result){
+            setQuestion(result.items[0]);
+            setLoading(false);
+        }
+    }
+    id && fetchData();
+  },[id])
   return (
     <QuestionDetailContainer>
-      <h2>Question: {id}</h2>
+      {loading? (
+        <span>Loading...</span>
+      ):(
+        <Card
+            title={question.title}
+            views={question.view_count}
+            answers={question.answers_count} 
+        />
+      )}
     </QuestionDetailContainer>
   );
 }
